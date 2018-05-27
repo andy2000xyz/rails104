4a0929d94829454664b5a1f28b44ac69876dc27c
andy2000xyz <andy2000xyz@hotmail.com>
2018-05-27 09:13:03 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission , only: [:edit, :update, :destroy]
 
   def index
@@ -43,6 +43,31 @@ class GroupsController < ApplicationController
       redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本讨论版成功！"
+      else
+        flash[:warning] = "您已经是本讨论版成员了！"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+  def quit
+    @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flashe[:alert] = "已退出本讨论版！"
+      else
+        flashe[:warning] = "您不是本讨论版成员，怎么退出"
+      end
+
+      redirect_to group_path(@group)
+    end
 
   private
 
@@ -53,7 +78,6 @@ class GroupsController < ApplicationController
       redirect_to root_path, alert: "You have no permission."
     end
   end
-
 
   def group_params
     params.require(:group).permit(:title, :description)
